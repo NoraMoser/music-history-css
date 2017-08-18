a83dd23463c4980a95cbc1fe79e15f1d71dc72eb
Nora Moser <noramoser@Noras-MBP.hsd1.tn.comcast.net>
2017-08-17 20:10:52 -0500

    a little more

--- javascripts/songs.js
+++ javascripts/songs.js
@@ -64,10 +64,18 @@ genre: "Alternative"
 ];
 
 let contentArea = document.getElementById("song-container");
+let contentArea2 = document.getElementById("song-container2");
 
 for (i = 0; i < songs2.length; i++) {
 	console.log(songs2[i]);
 	contentArea.innerHTML += `<div class="Song1 specific-song">
+								<h2>${songs2[i].name}</h2>
+								<h4 class="song1list artist-name">${songs2[i].artist}</h4>
+								<h4 class="song1list album-name">${songs2[i].album}</h4>
+								<h4 class="song1list genre-type">${songs2[i].genre}</h4>
+								</div>`;
+
+	contentArea2.innerHTML += `<div class="Song1 specific-song">
 								<h2>${songs2[i].name}</h2>
 								<h4 class="song1list artist-name">${songs2[i].artist}</h4>
 								<h4 class="song1list album-name">${songs2[i].album}</h4>
